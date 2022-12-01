e6e28a8a6bb765ef3b08e44b3a9d2dc24f05fd54
Wouter Verlaek <wouter@gitpod.io>
2022-12-01 15:26:08 +0000

    errorf

--- components/image-builder-bob/pkg/proxy/proxy.go
+++ components/image-builder-bob/pkg/proxy/proxy.go
@@ -127,7 +127,7 @@ func (proxy *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	if repo == nil {
-		log.Error("Not found here? %s %d", alias, len(proxy.Aliases))
+		log.Errorf("Not found here? %s %d", alias, len(proxy.Aliases))
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 		return
 	}
